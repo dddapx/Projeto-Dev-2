7d398fd46992183a93c67421f0fbb4cbfc0ed9b7
Davi Barros <davi.bfdepaiva@gmail.com>
2026-04-29 11:15:49 +0000

    esqueleto spring

---
+++ projeto-semestre/src/main/java/dev2/projeto_semestre/ProjetoSemestreApplication.java
@@ -0,0 +1,13 @@
+package dev2.projeto_semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemestreApplication.class, args);
+	}
+
+}
---
+++ projeto-semestre/src/test/java/dev2/projeto_semestre/ProjetoSemestreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev2.projeto_semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSemestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

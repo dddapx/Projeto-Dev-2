d7276416b1b0accb1f0cc1ec23c2a1caeee2276f
Davi Barros <davi.bfdepaiva@gmail.com>
2026-05-20 14:19:20 +0000

    dependencias e primeiro DTO completo

--- projeto-semestre/src/main/java/dev2/projeto_semestre/dto/ResumoCarteiraDTO.java
+++ projeto-semestre/src/main/java/dev2/projeto_semestre/dto/ResumoCarteiraDTO.java
@@ -2,4 +2,30 @@ package dev2.projeto_semestre.dto;
 
 public class ResumoCarteiraDTO {
 
+    private String nomeCarteira;
+    private String nomeInvestidor;
+    private Double valorTotalInvestido;
+    private Double valorTotalAtual;
+
+    public ResumoCarteiraDTO() {
+    }
+
+    public ResumoCarteiraDTO(String nomeCarteira, String nomeInvestidor, Double valorTotalInvestido, Double valorTotalAtual) {
+        this.nomeCarteira = nomeCarteira;
+        this.nomeInvestidor = nomeInvestidor;
+        this.valorTotalInvestido = valorTotalInvestido;
+        this.valorTotalAtual = valorTotalAtual;
+    }
+
+    public String getNomeCarteira() { return nomeCarteira; }
+    public void setNomeCarteira(String nomeCarteira) { this.nomeCarteira = nomeCarteira; }
+
+    public String getNomeInvestidor() { return nomeInvestidor; }
+    public void setNomeInvestidor(String nomeInvestidor) { this.nomeInvestidor = nomeInvestidor; }
+
+    public Double getValorTotalInvestido() { return valorTotalInvestido; }
+    public void setValorTotalInvestido(Double valorTotalInvestido) { this.valorTotalInvestido = valorTotalInvestido; }
+
+    public Double getValorTotalAtual() { return valorTotalAtual; }
+    public void setValorTotalAtual(Double valorTotalAtual) { this.valorTotalAtual = valorTotalAtual; }
 }

ea5dfc92d09bd60acd5ffb16e2004775508e6173
Davi Barros <davi.bfdepaiva@gmail.com>
2026-05-20 14:33:36 +0000

    resumo da carteira dto e imports em controller

--- projeto-semestre/src/main/java/dev2/projeto_semestre/controller/CarteiraController.java
+++ projeto-semestre/src/main/java/dev2/projeto_semestre/controller/CarteiraController.java
@@ -2,6 +2,8 @@ package dev2.projeto_semestre.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import dev2.projeto_semestre.dto.CarteiraRequestDTO;
 import dev2.projeto_semestre.dto.CarteiraResponseDTO;
 import dev2.projeto_semestre.service.CarteiraService;
+import dev2.projeto_semestre.dto.ResumoCarteiraDTO;
 
 @RestController
 @RequestMapping("/api/carteiras") 
--- projeto-semestre/src/main/java/dev2/projeto_semestre/service/CarteiraService.java
+++ projeto-semestre/src/main/java/dev2/projeto_semestre/service/CarteiraService.java
@@ -16,10 +16,15 @@ public class CarteiraService {
 
     private final CarteiraRepository carteiraRepository;
     private final InvestidorRepository investidorRepository;
+    private final TransacaoRepository transacaoRepository;
+    private final HgBrasilApiService hgBrasilApiService;
 
-    public CarteiraService(CarteiraRepository carteiraRepository, InvestidorRepository investidorRepository) {
+    public CarteiraService(CarteiraRepository carteiraRepository, InvestidorRepository investidorRepository,
+                           TransacaoRepository transacaoRepository,HgBrasilApiService hgBrasilApiService) {
         this.carteiraRepository = carteiraRepository;
         this.investidorRepository = investidorRepository;
+        this.transacaoRepository = transacaoRepository;
+        this.hgBrasilApiService = hgBrasilApiService;
     }
 
     public CarteiraResponseDTO criarCarteira(CarteiraRequestDTO dto) {
@@ -46,6 +51,50 @@ public class CarteiraService {
                 carteiraSalva.getId(),
                 carteiraSalva.getNome(),
                 carteiraSalva.getInvestidor().getNome()
+        );
+    }
+
+    public ResumoCarteiraDTO gerarResumo(Long id) {
+        
+        // busca a carteira no banco de dados para ver se ela existe
+        Carteira carteira = carteiraRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Carteira não encontrada!"));
+
+        List<Transacao> todasTransacoes = transacaoRepository.findAll();
+
+        double totalInvestido = 0.0;
+        double totalAtual = 0.0;
+
+        // faz um laço de repetição para olhar uma transação de cada vez
+        for (Transacao t : todasTransacoes) {
+            // verifica se essa transação pertence a carteira que estamos calculando
+            if (t.getCarteira().getId().equals(id)) {
+                
+                String codigoDaAcao = t.getAtivoFinanceiro().getCodigo();
+                int quantidade = t.getQuantidade();
+                double precoDeQuandoComprou = t.getPrecoOperacao();
+                
+                double precoDeHoje = hgBrasilApiService.buscarPrecoAtivo(codigoDaAcao);
+                
+                // se foi uma compra, soma o valor na carteira
+                if (t.getTipoOperacao().equalsIgnoreCase("COMPRA")) {
+                    totalInvestido += (quantidade * precoDeQuandoComprou);
+                    totalAtual += (quantidade * precoDeHoje);
+                } 
+                // se foi uma venda, diminui o valor dela
+                else if (t.getTipoOperacao().equalsIgnoreCase("VENDA")) {
+                    totalInvestido -= (quantidade * precoDeQuandoComprou);
+                    totalAtual -= (quantidade * precoDeHoje);
+                }
+            }
+        }
+
+        // monta o dto para devolver pro usuário com as respostas
+        return new ResumoCarteiraDTO(
+                carteira.getNome(),
+                carteira.getInvestidor().getNome(),
+                totalInvestido,
+                totalAtual
         );
     }
 

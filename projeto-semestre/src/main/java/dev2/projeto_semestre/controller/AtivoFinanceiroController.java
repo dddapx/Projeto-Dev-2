56d7fbaca150294c63de9057faeeba33042edc9d
Guilherme Miyamoto Bragatto <99208444+bragatto-tec@users.noreply.github.com>
2026-05-24 14:34:02 -0300

    adicionado delete de ativo

--- projeto-semestre/src/main/java/dev2/projeto_semestre/controller/AtivoFinanceiroController.java
+++ projeto-semestre/src/main/java/dev2/projeto_semestre/controller/AtivoFinanceiroController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,26 +21,32 @@ import dev2.projeto_semestre.service.AtivoFinanceiroService;
 @CrossOrigin(origins = "http://localhost:3000")
 public class AtivoFinanceiroController {
 
-        private final AtivoFinanceiroService service;
+    private final AtivoFinanceiroService service;
 
-        public AtivoFinanceiroController(AtivoFinanceiroService service) {
-            this.service = service;
-        }
+    public AtivoFinanceiroController(AtivoFinanceiroService service) {
+        this.service = service;
+    }
 
-        @PostMapping
-        public ResponseEntity<AtivoFinanceiro> criar(@RequestBody AtivoFinanceiro ativo){
+    @PostMapping
+    public ResponseEntity<AtivoFinanceiro> criar(@RequestBody AtivoFinanceiro ativo) {
         AtivoFinanceiro resultado = service.criarAtivo(ativo.getCodigo());
         return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
-        }
-
-        @GetMapping
-        public ResponseEntity<List<AtivoFinanceiro>> listar() {
-            return ResponseEntity.ok(service.listarTodos());
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<AtivoFinanceiro> buscarPorId(@PathVariable Long id) {
-            return ResponseEntity.ok(service.buscarAtivoPorId(id));
-        }
-
     }
+
+    @GetMapping
+    public ResponseEntity<List<AtivoFinanceiro>> listar() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AtivoFinanceiro> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarAtivoPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletarAtivo(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
